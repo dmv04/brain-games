26e436334fa4538ca2c9518bb80532494727ca36
DmitryVerchenko <verchenko.d.s@mail.ru>
2024-02-28 19:50:21 +0500

    Add Cli.java

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,13 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    public static void nameSetter() {
+        System.out.print("May I have your name? ");
+        Scanner scanner = new Scanner(System.in);
+        String userName = scanner.next();
+        System.out.println("Hello, " + userName + "!");
+        scanner.close();
+    }
+}
