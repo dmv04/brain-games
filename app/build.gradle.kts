545215e75abf903bb26c9e631a7210037dba198c
DmitryVerchenko <verchenko.d.s@mail.ru>
2024-03-01 19:56:53 +0500

    update gradle defaults

---
+++ app/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    id("java")
+    id ("application")
+    id("checkstyle")
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+
+application {
+    mainClass = "hexlet.code.App"
+}
+
+tasks.compileJava {
+    options.release = 20
+}
+
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+tasks.getByName("run", JavaExec::class) {
+    standardInput = System.`in`
+}
+
