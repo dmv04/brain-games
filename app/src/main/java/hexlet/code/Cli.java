6ddb854b14e879270f3d02fef0753046e2463fad
DmitryVerchenko <verchenko.d.s@mail.ru>
2024-03-01 19:58:36 +0500

    update Cli

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,11 +3,12 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
+    static String userName;
+
     public static void nameSetter() {
         System.out.print("May I have your name? ");
         Scanner scanner = new Scanner(System.in);
-        String userName = scanner.next();
+        userName = scanner.next();
         System.out.println("Hello, " + userName + "!");
-        scanner.close();
     }
 }
