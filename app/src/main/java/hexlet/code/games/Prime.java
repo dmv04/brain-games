c0425a63ea08db5aef2947b6b502f741a55e8a70
DmitryVerchenko <verchenko.d.s@mail.ru>
2024-03-08 21:22:33 +0500

    add Prime

---
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -0,0 +1,30 @@
+package hexlet.code.games;
+
+import hexlet.code.Utils;
+import java.util.Scanner;
+
+public class Prime {
+    public static void game() {
+        String userName = Utils.greetings();
+        System.out.println("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
+        for (int i = 0; i < 3; i++) {
+            int randomNumber = Utils.generateRandomNumber(0, 100);
+            Utils.qaText(randomNumber);
+            Scanner scanner = new Scanner(System.in);
+            String answer = scanner.next();
+            if ((Utils.isPrime(randomNumber) && answer.equals("yes")) || (!Utils.isPrime(randomNumber) && answer.equals("no"))) {
+                Utils.counter++;
+                System.out.println("Correct!");
+            } else if (Utils.isPrime(randomNumber) && answer.equals("no")) {
+                Utils.wrongAnswer(answer, "yes", userName);
+            } else if (!Utils.isPrime(randomNumber) && answer.equals("yes")) {
+                Utils.wrongAnswer(answer, "no", userName);
+            } else {
+                System.out.println("Wrong symbol");
+            }
+        }
+        if (Utils.counter == 3) {
+            System.out.println("Congratulations, " + userName);
+        }
+    }
+}
