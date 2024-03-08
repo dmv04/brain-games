76188f69b0147ac4aa091a10e8ca27208523a1a8
DmitryVerchenko <verchenko.d.s@mail.ru>
2024-03-08 21:22:18 +0500

    update Even

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -1,36 +1,33 @@
-package hexlet.code;
+package hexlet.code.games;
 
+import hexlet.code.Utils;
 import java.util.Scanner;
 
 public class Even {
     public static void game() {
-        Cli.nameSetter();
+        String userName = Utils.greetings();
         System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
         Scanner scanner = new Scanner(System.in);
-        int counter = 0;
         for (int i = 0; i < 3; i++) {
-            int randomNumber = (int) (Math.random() * 100);
-            System.out.println("Question: " + randomNumber);
-            System.out.print("Your answer: ");
+            int randomNumber = Utils.generateRandomNumber(0, 100);
+            Utils.qaText(randomNumber);
             String answer = scanner.next();
             if ((answer.equals("yes") && randomNumber % 2 == 0) || (answer.equals("no") && randomNumber % 2 == 1)) {
-                counter++;
+                Utils.counter++;
                 System.out.println("Correct!");
             } else if (answer.equals("yes") && randomNumber % 2 == 1) {
-                System.out.println("'yes' is wrong answer ;(. Correct answer was 'no'.");
-                System.out.println("Let's try again, " + Cli.userName);
+                Utils.wrongAnswer("yes", "no", userName);
                 break;
             } else if (answer.equals("no") && randomNumber % 2 == 0) {
-                System.out.println("'no' is wrong answer ;(. Correct answer was 'yes'.");
-                System.out.println("Let's try again, " + Cli.userName);
+                Utils.wrongAnswer("no", "yes", userName);
                 break;
             } else {
                 System.out.println("Wrong symbol");
                 break;
             }
         }
-        if (counter == 3) {
-            System.out.println("Congratulations, " + Cli.userName);
+        if (Utils.counter == 3) {
+            System.out.println("Congratulations, " + userName);
         }
     }
 }
